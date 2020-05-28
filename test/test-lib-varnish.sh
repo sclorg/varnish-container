6c1b898220af679bddae5f4ddf35efaf065a6d74
Honza Horak <hhorak@redhat.com>
2020-05-28 23:24:48 +0200

    Add tests for image streams and refactor tests to work with remote OCP4 cluster

--- test/test-lib-varnish.sh
+++ test/test-lib-varnish.sh
@@ -58,9 +58,38 @@ function test_varnish_integration() {
   VERSION=$version ct_os_test_s2i_app_func "${image_name}" \
                                            "https://github.com/sclorg/varnish-container.git" \
                                            test/test-app \
-                                           "test_response_redirect_internal 'http://<IP>:8080' '301' 'oldexample.com' 'http://example.org'" \
-                                           "" \
-                                           "${import_image}"
+                                           "test_response_redirect_internal 'http://<IP>:8080' '301' 'oldexample.com' 'http://example.org'"
 }
 
+# Check the imagestream
+function test_varnish_imagestream() {
+  local image_stream_file
+  local local_image_stream_file
+  case ${OS} in
+    rhel7|centos7) ;;
+    *) echo "Imagestream testing not supported for $OS environment." ; return 0 ;;
+  esac
+
+  image_stream_file="${THISDIR}/imagestreams/varnish-${OS}.json"
+  echo "Running image stream test for stream ${image_stream_file} and test application"
+
+  # shellcheck disable=SC2119
+  ct_os_new_project
+
+  local_image_stream_file=$(ct_obtain_input "${image_stream_file}")
+  oc create -f "${local_image_stream_file}"
+
+  # ct_os_test_s2i_app creates a new project, but we already need
+  # it before for the image stream import, so tell it to skip this time
+  CT_SKIP_NEW_PROJECT=true \
+  ct_os_test_s2i_app_func "${IMAGE_NAME}" \
+                          "https://github.com/sclorg/varnish-container.git" \
+                          test/test-app \
+                          "test_response_redirect_internal 'http://<IP>:8080' '301' 'oldexample.com' 'http://example.org'"
+
+  result=$?
+
+  # shellcheck disable=SC2119
+  ct_os_delete_project
+}
 # vim: set tabstop=2:shiftwidth=2:expandtab:
